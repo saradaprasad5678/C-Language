dfb6f6f44eb24844cfa59e62916feda4a2cdbed0
Sarada Prasad Das <117529826+saradaprasad5678@users.noreply.github.com>
2025-06-23 15:00:46 +0530

    Update A3.c

--- A3.c
+++ A3.c
@@ -176,3 +176,98 @@ int main() {
         printf("upppercase");
     return 0;
 }
+
+//13. Write a program to check whether a given number is divisible by 3 and divisible by 2.
+#include <stdio.h>
+int main() {
+    // Write C code here
+    int n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    if(n%2==0 && n%3==0)
+        printf("divisible by both");
+    else
+        printf("not divisible");
+    return 0;
+}
+
+//14. Write a program to check whether a given number is divisible by 7 or divisible by 3.
+#include <stdio.h>
+int main() {
+    // Write C code here
+    int n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    if(n%3==0 && n%7==0)
+        printf("divisible by both");
+    else if(n%3==0)
+        printf("divisible by 3");
+    else if(n%7==0)
+        printf("divisible by 7");
+    else
+        printf("not divisible by any");
+    return 0;
+}
+
+//15. Write a program to check whether a given number is positive, negative or zero.
+#include <stdio.h>
+int main() {
+    // Write C code here
+    int n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    if(n>0)
+        printf("positive");
+    else if(n<0)
+        printf("negative");
+    else
+        printf("its zero");
+    return 0;
+}
+
+//16. Write a program to check whether a given character is an alphabet (uppercase), an alphabet (lower case), a digit or a special character.
+#include <stdio.h>
+int main() {
+    // Write C code here
+    char n;
+    printf("enter the character:");
+    scanf("%c",&n);
+    if(n>='0' && n<='9')
+        printf("number");
+    else if(n>='a'&& n<='z')
+        printf("lowercase");
+    else if(n>='A'&& n<='Z')
+        printf("uppercase");
+    else
+        printf("its special char");
+    return 0;
+}
+
+//17. Write a program which takes the length of the sides of a triangle as an input. Display whether the triangle is valid or not.
+#include <stdio.h>
+int main() {
+    // Write C code here
+    int a,b,c;
+    printf("enter the lengths:");
+    scanf("%d%d%d",&a,&b,&c);
+    if((a+b)>c && (b+c)>a && (a+c)>b)
+        printf("valid");
+    else
+        printf("invalid");
+    return 0;
+}
+
+//18. Write a program which takes the month number as an input and display number of days in that month.
+#include <stdio.h>
+int main() {
+    int n;
+    printf("enter the lengths:");
+    scanf("%d",&n);
+    if(n==1||n==3||n==5||n==7||n==8||n==10||n==12)
+        printf("31 days are there");
+    else if(n==2)
+        printf("28 days are there");
+    else
+    printf("30 days are there");
+    return 0;
+}
